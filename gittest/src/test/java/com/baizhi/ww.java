821eb561764862490a119c66ccdf4fd274f29c7b
llm197120 <570891387@qq.com>
2019-08-27 17:16:08 +0800

    ver 1.0 基础功能版

--- gittest/src/test/java/com/baizhi/ww.java
+++ gittest/src/test/java/com/baizhi/ww.java
@@ -7,4 +7,14 @@ package com.baizhi;
  * @create: 2019-08-27 16:05
  **/
 public class ww {
+    private String id;
+    private String name;
+    public void show(String id, String name){
+        if(id.length()>2){
+            System.out.println("id格式正确");
+        }
+        if(name.length()>=4&&name.length()<=40){
+            System.out.println("姓名格式正确");
+        }
+    }
 }
